794ff47698f1d59494d77d07535798c442e1599f
Glitch (exultant-notebook) <none>
2018-01-19 05:53:20 +0000

    💼🎖 Checkpoint script.js:596315/99

--- script.js
+++ script.js
@@ -4,17 +4,14 @@ var svg = d3.select("svg"),
  width = svg.attr("width"),
  height = svg.attr("height");
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+
 
 d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json", function(err, data) {
   if (err)
   {
     throw err;
   }
-
+  
 var nodes = svg.append("g")
         .attr("class", "nodes")
         .selectAll(".nodes")
@@ -29,7 +26,12 @@ var nodes = svg.append("g")
     .selectAll("line")
     .data(data.links)
     .enter().append("line")
-      .attr("stroke-width", 2);    
+      .attr("stroke-width", 2);  
+  
+  var simulation = d3.forceSimulation()
+  .force("charge", d3.forceManyBody())
+  .force("center", d3.forceCenter(width / 2, height / 2))
+  .force("link", d3.forceLink().id(function(d,i) { return i }))
   
   simulation.nodes(nodes).on("tick", ticked);
   
