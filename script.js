cb4ab1d27993a73a2f850ca44fd65dc0563cae36
Glitch (exultant-notebook) <none>
2018-01-19 05:22:38 +0000

    🍖💦 Checkpoint script.js:596315/235

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 var d3;
+
+var svg = d3.select("svg"),
+ margin =100,
+ width = svg.attr("width")-margin,
+ height = svg.attr("height")-margin;
+
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("charge", d3.forceManyBody())
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+d3.json("https://raw.githubusercontent.com/DealPete/forceDirected/master/countries.json", function(err, data) {
+  if (err)
+  {
+    throw err;
+  }
+
+var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(data.nodes)
+        .enter()
+        .append("circle")
+        .attr("r", 5)
+        .attr("fill", "red");
+
+});
